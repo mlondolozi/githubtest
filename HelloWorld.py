a9a8f6e5c5cdf1548b8190c7461683ea96bbb031
mlondolozi <nikiwe.hlophe@gmail.com>
2024-05-06 11:51:34 +0200

    dictionaries
    dictionary staff

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,34 @@
 print("Hello world")
 for x in range(10):
-    print(f'testing {x}' )
+    print(f'testing {x}')
+
+
+#dictionaries
+person = {
+        'first_name':'Asabeneh',
+        'last_name':'Yetayeh',
+        'age':250,
+        'country':'Finland',
+        'is_marred':True,
+        'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+        'address':{
+            'street':'Space street',
+            'zipcode':'02210'
+        }
+    }
+
+print(person['address'])
+
+person['job_title'] = 'Instructor'
+person['skills'].append('HTML')
+print(person)
+
+print()
+person['first_name'] = 'Eyob'
+person['age'] = 252
+print(person)
+
+print()
+print(person.items())
+print()
+print(person.values())
