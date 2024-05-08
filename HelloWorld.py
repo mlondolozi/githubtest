8c1b6afa87b951dc6774be857270839a84f6b92b
mlondolozi <nikiwe.hlophe@gmail.com>
2024-05-08 08:17:33 +0200

    lambda functions
    small functions

--- HelloWorld.py
+++ HelloWorld.py
@@ -65,3 +65,15 @@ list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 print(list_of_lists)
 flattened_list = [ number for row in list_of_lists for number in row]
 print(flattened_list)
+
+add_two_nums = lambda a, b: a + b
+print(add_two_nums(2,3)) 
+ 
+# Self invoking lambda function
+(lambda a, b: a + b)(2,3) # 5 - need to encapsulate it in print() to see the result in the console
+
+def power(x):
+    return lambda n : x ** n
+
+cube = power(2)(3)   # function power now need 2 arguments to run, in separate rounded brackets
+print(cube)          # 8
