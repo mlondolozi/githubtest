2255679bb50e98b4f17380716365b6f0eb71a160
mlondolozi <nikiwe.hlophe@gmail.com>
2024-05-10 07:36:36 +0200

    detecting on image
    detecting on image

--- FaceDetection.py
+++ FaceDetection.py
@@ -1,4 +1,5 @@
 import cv2
+from random import randrange
 
 #this is actually where the training happens 
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -9,10 +10,12 @@ greyscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 #using the trained data to detect a face of the grey scaled image
 face_coordinates = trained_face_data.detectMultiScale(greyscaled_img)
-print(face_coordinates)
+#print(face_coordinates)
 
-#draw coordinates on image 
-cv2.rectangle(img, (533,373), (533+337,373+337), (0,255,255))
+#draw coordinates on images for each detect image
+for (x,y,w,h) in face_coordinates:
+    cv2.rectangle(img, (x, y, x+w, y+h), (randrange(255),randrange(255),randrange(255)), 5)
+
 cv2.imshow('Show the selected image', img)
 cv2.waitKey()
 
