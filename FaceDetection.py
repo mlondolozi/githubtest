a4ee9d693b0d1488fea0f489c2cddbeba48f5ccb
mlondolozi <nikiwe.hlophe@gmail.com>
2024-05-10 17:47:04 +0200

    detect from camera
    detect from camera

--- FaceDetection.py
+++ FaceDetection.py
@@ -4,10 +4,29 @@ from random import randrange
 #this is actually where the training happens 
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.imread('Nikiwe.jpg')
+#img = cv2.imread('Nikiwe.jpg')
+#capture video from webcam 
+webcam = cv2.VideoCapture(0)
 
-greyscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#loop over video frames 
+while True: 
+    #read current frame 
+    succesfull_frame_read, frame = webcam.read()
 
+    greyscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    face_coordinates = trained_face_data.detectMultiScale(greyscaled_img)
+
+    for (x,y,w,h) in face_coordinates:
+        cv2.rectangle(frame, (x, y, x+w, y+h), (randrange(255),randrange(255),randrange(255)), 5)
+
+    cv2.imshow('Show the selected image', frame)
+    key = cv2.waitKey(1)
+
+    if key == 81 or key == 113:
+        break
+
+'''
 #using the trained data to detect a face of the grey scaled image
 face_coordinates = trained_face_data.detectMultiScale(greyscaled_img)
 #print(face_coordinates)
@@ -18,5 +37,5 @@ for (x,y,w,h) in face_coordinates:
 
 cv2.imshow('Show the selected image', img)
 cv2.waitKey()
-
+'''
 print('Code Complete!!!')
